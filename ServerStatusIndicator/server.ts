deca3f9754d787ff32b0ad136c72ec5bc6e1c84c
nicholasceliano <nicholasceliano@yahoo.com>
2015-06-29 10:48:28 -0400

    Migration off of TFS

---
+++ ServerStatusIndicator/server.ts
@@ -0,0 +1,16 @@
+﻿import http = require('http');
+var port = process.env.port || 1337;
+
+var jade = require('jade'); //this is breaking it on work computer....retarted
+
+http.createServer(function (req, res) {
+
+    
+    var fn = jade.compile('string of jade');
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+    res.write(fn);
+
+    res.end('Hello World\n' + fn);
+}).listen(port);
